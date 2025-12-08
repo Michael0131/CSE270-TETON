96158d736d6c645d4b2e904bf46a4fcabf9e3fb9
Michael0131 <michaelj0131@byui.edu>
2025-12-08 16:55:16 -0700

    bug with user invalid. fixed

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,23 +3,17 @@ import requests
 BASE_URL = "https://michael0131.github.io/CSE270-TETON"
 
 def test_users_valid_credentials():
-    response = requests.get(f"{BASE_URL}/data/users.json")
+    response = requests.get(f"{BASE_URL}/users/", params={
+        "username": "admin",
+        "password": "qwerty"
+    })
     assert response.status_code == 200
-
-    users = response.json()
-
-    # check that admin exists
-    assert "admin" in users
-
-    # verify correct password
-    assert users["admin"]["password"] == "qwerty"
-
+    assert response.text == ""
 
 def test_users_invalid_credentials():
-    response = requests.get(f"{BASE_URL}/data/users.json")
-    assert response.status_code == 200
-
-    users = response.json()
-
-    # invalid username should not be present
-    assert "not_a_real_user" not in users
+    response = requests.get(f"{BASE_URL}/users/", params={
+        "username": "admin",
+        "password": "admin"
+    })
+    assert response.status_code == 401
+    assert response.text == ""
