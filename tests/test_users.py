3bb4e85cf99fbea042a0fd68542415a6c9002166
Michael0131 <michaelj0131@byui.edu>
2025-12-08 16:45:14 -0700

    updated test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,21 @@
 import requests
 
-BASE_URL = "https://michael0131.github.io/CSE270-TETON/"
+BASE_URL = "https://michael0131.github.io/CSE270-TETON/teton/1.6"
 
 def test_users_valid_credentials():
-    response = requests.get(f"{BASE_URL}/users/", params={
-        "username": "admin",
-        "password": "qwerty"
-    })
+    response = requests.get(f"{BASE_URL}/data/users.json")
     assert response.status_code == 200
-    assert response.text == ""
+
+    users = response.json()
+
+    assert "admin" in users
+    assert users["admin"]["password"] == "qwerty"
+
 
 def test_users_invalid_credentials():
-    response = requests.get(f"{BASE_URL}/users/", params={
-        "username": "admin",
-        "password": "admin"
-    })
-    assert response.status_code == 401
-    assert response.text == ""
+    response = requests.get(f"{BASE_URL}/data/users.json")
+    assert response.status_code == 200
+
+    users = response.json()
+
+    assert "not_a_real_user" not in users
