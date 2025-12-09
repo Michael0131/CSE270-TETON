581e23270e610b4ef978c3bfb21c312dd2e433b6
Michael0131 <michaelj0131@byui.edu>
2025-12-08 17:01:54 -0700

    idk

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,9 @@
 import requests
 
-BASE_URL = "https://michael0131.github.io/CSE270-TETON"
+BASE_URL = "http://127.0.0.1:8000"
 
 def test_users_valid_credentials():
-    response = requests.get(f"{BASE_URL}/directorydata_service/users", params={
+    response = requests.get(f"{BASE_URL}/users/", params={
         "username": "admin",
         "password": "qwerty"
     })
